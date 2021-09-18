1ab95be2633138d8c0228f9b28254692e858c7b2
Gary DeReese <garydereese@sbcglobal.net>
2021-09-17 19:39:10 -0500

    refactor(model): changed to normal classes

--- src/tonescript/model.py
+++ src/tonescript/model.py
@@ -4,14 +4,16 @@ Object model for representing the structure and properties of ToneScripts and th
 
 from dataclasses import dataclass
 from decimal import Decimal
+from typing import Iterable
 from typing import Sequence
 
 
 @dataclass
 class ToneSegment:
-    duration_on: Decimal
-    duration_off: Decimal
-    freq_nums: Sequence[int]
+    def __init__(self, duration_on: Decimal = None, duration_off: Decimal = None, freq_nums: Iterable[int] = None):
+        self.duration_off = duration_off
+        self.duration_on = duration_on
+        self._freq_nums = list(freq_nums)
 
     def __str__(self):
         if self.duration_on.is_infinite():
@@ -26,11 +28,16 @@ class ToneSegment:
 
         return f"{self.__class__.__name__} on={duration_on} off={duration_off} frequencies={self.freq_nums}>"
 
+    @property
+    def freq_nums(self) -> Sequence[int]:
 
-@dataclass
+        return self._freq_nums
+
+
 class CadenceSection:
-    duration: Decimal
-    segments: Sequence[ToneSegment]
+    def __init__(self, duration: Decimal = None, segments: Iterable[ToneSegment] = None):
+        self.duration = duration
+        self.segments = list(segments)
 
     def __str__(self):
         if self.duration.is_infinite():
@@ -43,7 +50,8 @@ class CadenceSection:
 
 @dataclass
 class CadScript:
-    sections: Sequence[CadenceSection]
+    def __init__(self, sections: Iterable[CadenceSection] = None):
+        self.sections = list(sections)
 
     def __str__(self):
         return f"<{self.__class__.__name__} sections={len(self.sections)}>"
@@ -51,8 +59,9 @@ class CadScript:
 
 @dataclass
 class FrequencyComponent:
-    frequency: int
-    level: Decimal
+    def __init__(self, frequency: int = None, level: Decimal = None):
+        self.frequency = frequency
+        self.level = level
 
     def __str__(self):
         return f"<{self.__class__.__name__} frequency={self.frequency}Hz level={self.level:.1g}dBm>"
@@ -60,7 +69,8 @@ class FrequencyComponent:
 
 @dataclass
 class FreqScript:
-    components: Sequence[FrequencyComponent]
+    def __init__(self, components: Iterable[FrequencyComponent] = None):
+        self.components = list(components)
 
     def __str__(self):
         return f"<{self.__class__.__name__} components={len(self.components)}>"
@@ -68,8 +78,9 @@ class FreqScript:
 
 @dataclass
 class ToneScript:
-    freqscript: FreqScript
-    cadscript: CadScript
+    def __init__(self, freqscript: FreqScript = None, cadscript: CadScript = None):
+        self.cadscript = cadscript or CadScript()
+        self.freqscript = freqscript or FreqScript()
 
     def __str__(self):
         lines = []
