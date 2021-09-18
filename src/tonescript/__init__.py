f33c71faffec094be15c71a723835f541f9bbbaa
Gary DeReese <garydereese@sbcglobal.net>
2021-09-17 19:47:43 -0500

    docs(index): moved to package folder

--- src/tonescript/__init__.py
+++ src/tonescript/__init__.py
@@ -2,7 +2,7 @@
 Python package for working with ToneScript, a syntax for describing the characteristics of the
 call progress tones used in telephony.
 
-.. include:: ../../docs/index.md
+.. include:: index.md
 """
 
 from ._parser import parse
